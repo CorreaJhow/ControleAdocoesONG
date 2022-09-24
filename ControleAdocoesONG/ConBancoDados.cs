56e047bcf18bf8ddb1a54965d6c55cd52e198ee2
Jhonatas Rodrigues Correa <jhonatasrcorrea@gmail.com>
2022-09-24 17:58:04 -0300

    Menu adicionado com verificações (inicio de classes)

---
+++ ControleAdocoesONG/ConBancoDados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleAdocoesONG
+{
+    internal class ConBancoDados
+    {
+        string Conexao = "Data Source=localhost; Initial Catalog=ONG_Adocoes; User Id=sa; Password=12345;";
+        public ConBancoDados()
+        {
+
+        }
+        public string Caminho()
+        {
+            return Conexao;
+        }
+    }
+}
--- ControleAdocoesONG/Program.cs
+++ ControleAdocoesONG/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ControleAdocoesONG
 {
@@ -6,7 +7,135 @@ namespace ControleAdocoesONG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+        public static void Menu()
+        {
+            do
+            {
+                CabecalhoONG();
+                Console.WriteLine("Bem vindo ao sistema de Adoções da nossa ONG");
+                Console.WriteLine("O que deseja realizar: \n[0]Sair\n[1]Registrar \n[2]Atualizar \n[3]Consultar"); //pensar sobre o delete
+                int opcao = int.Parse(Console.ReadLine());
+                while (opcao < 0 || opcao > 3)
+                {
+                    Console.WriteLine("Opção inválida informada, por favor digite novemente!");
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                switch (opcao)
+                {
+                    case 0:
+                        Console.Clear();
+                        CabecalhoONG();
+                        Console.WriteLine("Obrigado pela visita!");
+                        Thread.Sleep(1000);
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        bool volta = false;
+                        do
+                        {
+                            Console.Clear();
+                            CabecalhoONG();
+                            Console.WriteLine("O que voce deseja Registrar: \n[0]Voltar \n[1]Pessoa (cliente) \n[2]Animal ");
+                            int op1 = int.Parse(Console.ReadLine());
+                            while (op1 != 1 || op1 != 2)
+                            {
+                                Console.WriteLine("Valor inválido informado, tente novamente: ");
+                                op1 = int.Parse(Console.ReadLine());
+                            }
+                            switch (op1)
+                            {
+                                case 0:
+                                    volta = true;
+                                    //voltar
+                                    break;
+                                case 1:
+                                    //inserir pessoas
+                                    break;
+                                case 2:
+                                    //insert into animal
+                                    break;
+                                default:
+                                    Console.WriteLine("Opção inválida informada");
+                                    break;
+                            }
+                        } while (!volta);
+                        break;
+                    case 2:
+                        bool volta2 = false;
+                        do
+                        {
+                            Console.Clear();
+                            CabecalhoONG();
+                            Console.WriteLine("O que voce deseja Atualizar: \n[0]Voltar\n[1]Pessoa (cliente) \n[2]Animal ");
+                            int op2 = int.Parse(Console.ReadLine());
+                            while (op2 < 0 || op2 > 2)
+                            {
+                                Console.WriteLine("Valor inválido informado, tente novamente: ");
+                                op2 = int.Parse(Console.ReadLine());
+                            }
+                            switch (op2)
+                            {
+                                case 0:
+                                    volta2 = true;
+                                    //voltar
+                                    break;
+                                case 1:
+                                    //update Pessoa
+                                    break;
+                                case 2:
+                                    //update into animal
+                                    break;
+                                default:
+                                    Console.WriteLine("Opção inválida informada");
+                                    break;
+                            }
+                        } while (!volta2);
+                        break;
+                    case 3:
+                        bool volta3 = false;
+                        do
+                        {
+                            Console.Clear();
+                            CabecalhoONG();
+                            Console.WriteLine("O que voce deseja Consultar: \n[0]Voltar\n[1]Pessoa (cliente) \n[2]Animal ");
+                            int op3 = int.Parse(Console.ReadLine());
+                            while (op3 < 0 || op3 > 2)
+                            {
+                                Console.WriteLine("Valor inválido informado, tente novamente: ");
+                                op3 = int.Parse(Console.ReadLine());
+                            }
+                            switch (op3)
+                            {
+                                case 0:
+                                    volta3 = true;
+                                    //voltar
+                                    break;
+                                case 1:
+                                    //select Pessoa
+                                    break;
+                                case 2:
+                                    //select into animal
+                                    break;
+                                default:
+                                    Console.WriteLine("Opção inválida informada");
+                                    break;
+                            }
+                        } while (!volta3);
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida informada");
+                        break;
+                }
+            } while (true);
+        }
+
+        private static void CabecalhoONG()
+        {
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("                                                  ONG - Paraíso dos Animais");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
         }
     }
 }
