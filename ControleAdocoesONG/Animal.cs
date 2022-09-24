19d6d71b79f5b8424264916f092bd78b83bf729f
Jhonatas Rodrigues Correa <jhonatasrcorrea@gmail.com>
2022-09-24 18:17:54 -0300

    Classes [Pessoa, Animal, ConBancoDados] adicionadas

---
+++ ControleAdocoesONG/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ControleAdocoesONG
+{
+    internal class Animal
+    {
+        public int Chip { get; set; }
+        public string Familia { get; set; }
+        public string Raca { get; set; }
+        public string  Sexo { get; set; }
+        public string Nome { get; set; }
+        public Pessoa Adotante { get; set; }
+        public Animal()
+        {
+
+        }
+        public Animal(int chip, string familia, string raca, string sexo, string nome, Pessoa adotante)
+        {
+            Chip = chip;
+            Familia = familia;
+            Raca = raca;
+            Sexo = sexo;
+            Nome = nome;
+            Adotante = adotante;
+        }
+    }
+}
--- ControleAdocoesONG/Pessoa.cs
+++ ControleAdocoesONG/Pessoa.cs
@@ -1,13 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ControleAdocoesONG
 {
     internal class Pessoa
     {
-        public int MyProperty { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public char Sexo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string SiglaEstado { get; set; }
+        public string Numero { get; set; }
+        public Pessoa()
+        {
+
+        }
+        public Pessoa(string nome, string cPF, char sexo, DateTime dataNascimento, string telefone, string rua, string bairro, string cidade, string siglaEstado, string numero)
+        {
+            Nome = nome;
+            CPF = cPF;
+            Sexo = sexo;
+            DataNascimento = dataNascimento;
+            Telefone = telefone;
+            Rua = rua;
+            Bairro = bairro;
+            Cidade = cidade;
+            SiglaEstado = siglaEstado;
+            Numero = numero;
+        }   
     }
 }
